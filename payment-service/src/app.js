d156262441d1c6126ced123b563a68609a1950cc
Akash Agrawal <agrawalakash82@gmail.com>
2024-11-24 18:15:52 +0530

    payment service app.js updated

--- payment-service/src/app.js
+++ payment-service/src/app.js
@@ -17,6 +17,6 @@ listenForPayments();
 
 
 app.listen(PORT , ()=>{
-    console.log(`payment service on port ${PORT}`);
+    console.log(`payment service on port ${PORT} and ci/cd working`);
     
 })
